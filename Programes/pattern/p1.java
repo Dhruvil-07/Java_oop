09c96034fec53238a314449fbff90134749119d3
Dhruvil Shah <dhruvilshah732@gmail.com>
2024-01-07 22:32:27 +0530

    some update

--- Programes/pattern/p1.java
+++ Programes/pattern/p1.java
@@ -262,5 +262,20 @@ class piramid
     }
 
 
+
+/*
+    void piramid1()
+    {
+        for(int i = 1 ; i<=row ; i++)
+        {
+            for(int j = 1 ; j<=i ; j++) 
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+*/
+
 }
 
